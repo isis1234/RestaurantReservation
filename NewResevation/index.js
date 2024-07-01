ff0f099f1ce7533121e71c75646007550b4bddfd
Isis Tang <v176ist@cathaypacific.com>
2024-07-02 01:16:01 +0800

    done

--- NewResevation/index.js
+++ NewResevation/index.js
@@ -2,37 +2,37 @@ require("dotenv").config()
 const { MongoClient } = require('mongodb');
 
 const getDBConnection = async (event, context) => {
-  try {
-    const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.MONGO_DB}`;
-    const client = new MongoClient(url);
-    return client;
-  } catch(err) {
-      throw(err)
-  }
+    try {
+      const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=RestaurantReservation`;
+      console.log(url)
+      const client = new MongoClient(url);
+      return client;
+    } catch(err) {
+        throw(err)
+    }
 }
 
 const main = async function(event, context){
-  // event = {
-  //   body: JSON.stringify({
-  //     restaurant_name: "挽肉と米",
-  //     reg_people: 4,
-  //     reg_date: "2024-07-11",
-  //     reg_time: "7pm",
-  //     contact_method: "telegram",
-  //     telegram: "uuuuuzx",
-  //     whatsapp: null,
-  //   })
-  // };
+  event = {
+    body: JSON.stringify({
+      restaurant_name: "挽肉と米",
+      reg_people: 4,
+      reg_date: "2024-07-11",
+      reg_time: "7pm",
+      contact_method: "telegram",
+      telegram: "uuuuuzx",
+      whatsapp: null,
+    })
+  };
   const mongo = await getDBConnection();
 
   try {
     // Parse the request body
     const requestBody = JSON.parse(event.body);
-    const createdDate = new Date();
-    // const {
-    //   restaurant_name, reg_people, reg_date, reg_time, contact_method, telegram, whatsapp,
-    //   created_date = new Date()
-    // } = requestBody;
+    const {
+      restaurant_name, reg_people, reg_date, reg_time, contact_method, telegram, whatsapp,
+      created_date = new Date()
+    } = requestBody;
 
     // Insert record
     await mongo.connect();
@@ -42,7 +42,10 @@ const main = async function(event, context){
     const response = {
       statusCode: 200,
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": process.env.ORIGIN,
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
       },
       body: JSON.stringify({
         requestBody, created_date,
